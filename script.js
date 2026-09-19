5484ebacf4f7e897fb03bcc4395681d5cb769556
ama649 <ama649@uclive.ac.nz>
2026-09-19 16:31:18 +1200

    tiny change

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ button.addEventListener('click', connectBluetooth);
 
 function handlePressureData(event) {
     const value = event.target.value;
-    
-    const pressure = value.getFloat64(0, true); 
-    
-    // 3. Update your HTML text (formatting it to 2 decimal places so it looks clean)
-    document.getElementById('pressure_display').innerText = pressure.toFixed(2) + " PSI";
+    if (valueView.byteLength >= 8) {
+      const pressure = value.getFloat64(0, true); 
+      // 3. Update your HTML text (formatting it to 2 decimal places so it looks clean)
+      document.getElementById('pressure_display').innerText = pressure.toFixed(2) + " PSI";
+    }
 }
 
 async function connectBluetooth() {
